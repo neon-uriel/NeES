7788019b6897e210ad2010afdec83e9eb1e468c1
neon-uriel <48177204+neon-uriel@users.noreply.github.com>
2023-11-09 03:03:41 +0900

    wrote about CPU and OpCodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod cpu;
+mod opcodes;
+
+#[macro_use]
+extern crate lazy_static;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/opcodes.rs
@@ -0,0 +1,56 @@
+use crate::cpu::AddressingMode;
+use std::collections::HashMap;
+
+pub struct OpCode {
+    pub code: u8,
+    pub mnemonic: &'static str,
+    pub len: u8,
+    pub cycles: u8,
+    pub mode: AddressingMode,
+}
+
+impl OpCode {
+    fn new(code: u8, mnemonic: &'static str, len: u8, cycles: u8, mode: AddressingMode) -> Self {
+        OpCode {
+            code: code,
+            mnemonic: mnemonic,
+            len: len,
+            cycles: cycles,
+            mode: mode,
+        }
+    }
+}
+
+
+lazy_static! {
+    pub static ref CPU_OPS_CODES: Vec<OpCode> = vec![
+        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+        OpCode::new(0xE8, "INX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xAA, "TAX", 1, 2, AddressingMode::NoneAddressing),
+        
+        OpCode::new(0xA9, "LDA", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xA5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xB5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0xAD, "LDA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xBD, "LDA", 3, 4, AddressingMode::Absolute_X),
+        OpCode::new(0xB9, "LDA", 3, 4, AddressingMode::Absolute_Y),
+        OpCode::new(0xA1, "LDA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(0xB1, "LDA", 2, 5, AddressingMode::Indirect_Y),
+        
+        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0x8D, "STA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x9D, "STA", 3, 5, AddressingMode::Absolute_X),
+        OpCode::new(0x99, "STA", 3, 5, AddressingMode::Absolute_Y),
+        OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
+    ];
+
+    pub static ref OPCODES_MAP: HashMap<u8, &'static OpCode> = {
+        let mut map = HashMap::new();
+        for cpuop in &*CPU_OPS_CODES {
+            map.insert(cpuop.code, cpuop);
+        }
+        map
+    };
+}
